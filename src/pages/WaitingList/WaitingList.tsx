aad8516b20cd2b8efbbe175ee5f930b44d76803f
HAERYN <syt06119@naver.com>
2023-01-05 17:56:35 +0900

    feat: #3 로고 삽입

--- src/pages/WaitingList/WaitingList.tsx
+++ src/pages/WaitingList/WaitingList.tsx
@@ -1,7 +1,9 @@
 import { Container } from "@material-ui/core";
 import React from "react";
 
+
 import ListTable from "../../components/WaitingList/ListTable";
+import Logo from "../../components/WaitingList/Logo";
 import SubButton from "../../components/WaitingList/SubButton";
 import TableTitle from "../../components/WaitingList/TableTitle";
 
@@ -11,6 +13,9 @@ function WaitingList() {
 
   return (
     <Container style={{display: 'flex', overflow: 'hidden'}}>
+      <div>
+        <Logo/>
+      </div>
       <div>
         <TableTitle/>
         <ListTable/>
