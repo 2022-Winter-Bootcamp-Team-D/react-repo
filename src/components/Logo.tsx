a28df4b069227600d0f86c7bbf9dcdc72c6d32de
mina <kmaoo0406@naver.com>
2023-01-05 16:07:23 +0900

    Feat: #5 로고 이미지 삽입

---
+++ src/components/Logo.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import LogoImage from '../../assets/image/LogoImage.png'
+
+function Logo() {
+    return(
+    <div>
+        <img src={LogoImage} />
+    </div>
+    )
+}
+export default Logo;
--- src/pages/CustomerRegister/CustomerRegister.tsx
+++ src/pages/CustomerRegister/CustomerRegister.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-//import Logo from "../../components/CustomerRegister/Logo";
+import Logo from "../../components/Logo";
 //import RegisterButton from "../../components/CustomerRegister/RegisterButton";
 //import RegisterInput from "../../components/CustomerRegister/RegisterInput";
 
 function CustomerRegister() {
     document.body.style.backgroundColor = "#FFFBD9";
-    return(<div>ss</div>);
+    return(<div><Logo/></div>);
   }
 
-  export default CustomerRegister;
+  export default CustomerRegister;  //import를 하기 위해선 export(내보내기)를 해야 한다
