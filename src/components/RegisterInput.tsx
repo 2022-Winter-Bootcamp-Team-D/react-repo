4861ff835928c6d5ad049f7b312c3a9eb77bd0b8
mineii <kmaoo0406@naver.com>
2023-01-07 00:57:50 +0900

    feat: #3 RegisterInput 추가

---
+++ src/components/RegisterInput.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import './RegisterInput.scss';
+
+export default function RegisterInput() {
+  return (
+    <Box className="InputStyle"
+      component="form"
+      sx={{
+        '& > :not(style)': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField id="standard-basic" label="가게명/업소명" variant="standard" />
+      <TextField id="standard-basic" label="가게 번호" variant="standard" />
+      <TextField id="standard-basic" label="가게 주소" variant="standard" />
+      <TextField id="standard-basic" label="비밀번호(4자리)" variant="standard" />
+    </Box>
+  );
+}
--- src/pages/CustomerRegister/CustomerRegister.tsx
+++ src/pages/CustomerRegister/CustomerRegister.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 
 import Logo from "../../components/Logo";
-//import RegisterButton from "../../components/CustomerRegister/RegisterButton";
-//import RegisterInput from "../../components/CustomerRegister/RegisterInput";
+//import RegisterButton from "../../components/RegisterButton";
+import RegisterInput from "../../components/RegisterInput";
 
 function CustomerRegister() {
     document.body.style.backgroundColor = "#FFFBD9";
-    return(<div><Logo/></div>);
+    return(<div>
+      <Logo/>
+      <RegisterInput/>
+      </div>);
   }
 
   export default CustomerRegister;  //import를 하기 위해선 export(내보내기)를 해야 한다
