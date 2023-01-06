477ad563c57301a3ec1be6d7ab8fb03321cb3b02
mineii <kmaoo0406@naver.com>
2023-01-07 01:32:17 +0900

    feat: #3 가게등록 버튼 추가

--- src/components/RegisterInput.tsx
+++ src/components/RegisterInput.tsx
@@ -5,18 +5,22 @@ import './RegisterInput.scss';
 
 export default function RegisterInput() {
   return (
-    <Box className="InputStyle"
-      component="form"
-      sx={{
-        '& > :not(style)': { m: 1, width: '25ch' },
-      }}
-      noValidate
-      autoComplete="off"
-    >
-      <TextField id="standard-basic" label="가게명/업소명" variant="standard" />
-      <TextField id="standard-basic" label="가게 번호" variant="standard" />
-      <TextField id="standard-basic" label="가게 주소" variant="standard" />
-      <TextField id="standard-basic" label="비밀번호(4자리)" variant="standard" />
-    </Box>
+    <div>
+        <Box className="InputStyle"
+        component="form"
+        sx={{
+            '& > :not(style)': { m: 1, width: '25ch' },
+        }}
+        noValidate
+        autoComplete="off"
+        >
+        <TextField id="standard-basic" label="가게명/업소명" variant="standard" />
+        <TextField id="standard-basic" label="가게 번호" variant="standard" />
+        <TextField id="standard-basic" label="가게 주소" variant="standard" />
+        <TextField id="standard-basic" label="비밀번호(4자리)" variant="standard" />
+        </Box>
+        <button className="ButtonStyle" >가게등록</button>
+    </div>
+
   );
 }
