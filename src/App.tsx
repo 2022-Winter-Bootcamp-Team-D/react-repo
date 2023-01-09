9b27fde9d93a403023437bb74e8c7522d4b4a25e
“mineii” <kmaoo0406@naver.com>
2023-01-09 21:39:23 +0900

    fix: #3 충돌 해결

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
 import React from 'react';
 import './App.css';
-<<<<<<< HEAD
-=======
-import CustomerRegister from './pages/CustomerRegister/CustomerRegister';
-import Login from './pages/CustomerRegister/Login';
->>>>>>> develop
 import {
   BrowserRouter as Router,
   Routes,
   Route,
-<<<<<<< HEAD
 } from "react-router-dom";
 
 // import CustomerRegister from "./pages/CustomerRegister/CustomeRegister";
@@ -17,21 +11,13 @@ import {
 import WaitingList from "./pages/WaitingList/WaitingList";
 import Calender from './components/WaitingList/Calender';
 import StoreInformation from "././components/WaitingList/StoreInformation";
-=======
-} from "react-router-dom";        // 라우터를 사용하기 위한 
->>>>>>> develop
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
-<<<<<<< HEAD
           <Route path="/" element={<WaitingList />} />
-=======
-          <Route path="/" element={<Login />} />
-          <Route path="/register" element={<CustomerRegister />} />
->>>>>>> develop
         </Routes>
       </div>
     </Router>
