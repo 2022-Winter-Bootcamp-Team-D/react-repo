eec6598d9c4d2f8e5373bd54e9ec33be1b532116
“mineii” <kmaoo0406@naver.com>
2023-01-10 01:08:05 +0900

    feat: #3 StoreInformation 크기 및 너비 재지정

--- src/components/WaitingList/StoreInformation.tsx
+++ src/components/WaitingList/StoreInformation.tsx
@@ -4,15 +4,14 @@ import './StoreInformation.scss';
 
 export default function StoreInformation() {
   return (
-    //<div className='InformationInput'>
-    <TextareaAutosize
-      minRows={15}
-      maxRows={18}
-      aria-label="매장 소개 정보"
-      placeholder="매장 소개글을 입력해주세요."
-      style={{ width: 225 }}
-      className='InformationInput'
-    />
-    //</div>
+    <div className='InformationOutline'>
+      <TextareaAutosize
+        minRows={25}
+        maxRows={30}
+        aria-label="매장 소개 정보"
+        placeholder="매장 소개글을 입력해주세요."
+        className='InformationInput'
+      />
+    </div>
   );
 }
