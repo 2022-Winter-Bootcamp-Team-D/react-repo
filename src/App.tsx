17b14999bdfa95307f7be671e360641199bb835a
HAERYN <syt06119@naver.com>
2023-01-10 15:35:49 +0900

    fix: #3 conflict 오류 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,26 @@
 import React from 'react';
 import './App.css';
-<<<<<<< HEAD
-=======
-import StoreRegister from './pages/StoreRegister/StoreRegister';
-import Login from './pages/StoreRegister/Login';
->>>>>>> develop
 import {
   BrowserRouter as Router,
   Routes,
   Route,
 } from "react-router-dom";
 
-import CustomerRegister from "./pages/CustomerRegister/CustomerRegister";
+
 // import StoreRegister from "./pages/StoreRegister/StoreRegister";
 import WaitingList from "./pages/WaitingList/WaitingList";
 import Calender from './components/WaitingList/Calender';
 import StoreInformation from "././components/WaitingList/StoreInformation";
+import Login from './pages/CustomerRegister/Login';
+import CustomerRegister from './pages/CustomerRegister/CustomerRegister';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
-<<<<<<< HEAD
-          <Route path="/" element={<WaitingList />} />
-          <Route path="/customerregister" element={<CustomerRegister />} /> 
-=======
           <Route path="/" element={<Login />} />
-          <Route path="/storeRegister" element={<StoreRegister />} />
->>>>>>> develop
+          <Route path="/storeRegister" element={<CustomerRegister />} />
         </Routes>
       </div>
     </Router>
