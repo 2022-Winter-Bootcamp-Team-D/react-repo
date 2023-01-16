2e987863f71404982dfaafefc1241f089d2692c7
“mineii” <kmaoo0406@naver.com>
2023-01-17 03:45:03 +0900

    feat: #12 대기 등록 마감 API 완료 (대기마감/대기시작버튼)

--- src/components/WaitingList/SubButton.tsx
+++ src/components/WaitingList/SubButton.tsx
@@ -8,27 +8,14 @@ import axios from "axios";
 
 function SubButton() {
   
-  const [store_id, setStore_id] = useState(''); // 로그인 하고 받은 스토어 아이디 넣기
   const [is_waiting, setIs_waiting] = useState(true);  // 로그인 하고 받은 스토어 아이디 가지고 조회해서 이즈웨이팅 값 넣기
-
-  // useEffect(()=>{
-  //   // 스토어 아이디 세팅 => 로그인에서 받은 스토어 아이디
-  // })
-  
-
   const iswaitingChange = () => {
-
-    // 버튼을 누르면 스토어 아이디가 만약에 1이라고 하면, 스토어 아이디가 1인 정보에 가서 이즈웨이팅을 바꿔준다. patch
-
-    // 스토어 아디에 접근을 해서 이즈웨티잉을 가져와서 setWait로 바꿔준다.
-
-    // axios.patch('http://localhost:8000//api/v1/stores/breaktimes/',{
-    //   store_id: store_id  //로컬스토리지에서ㅓ get해오는 함수
-    // }).then(res=>
-    //     setIs_waiting(!is_waiting)  // 
-    // )
-
-    setIs_waiting(!is_waiting);
+    axios.patch('http://localhost:8000/api/v1/store/breaktime/',{
+      store_id: localStorage.getItem("store_id") //1 //회원가입(가게등록)을 해서 store_id가 생성이 돼야 localStorage에 저장이 되고,
+                                                    //이후 getItem을 통해 store_id를 가져올 수 있는데, 아직 그 과정이 선행되지 않아 임의로 1로 지정해주어 확인 작업함
+    })
+    .then(res => setIs_waiting(!is_waiting)  
+    )  
   }
 
   return (
@@ -48,7 +35,7 @@ function SubButton() {
           {is_waiting ? 'CLOSE' : 'OPEN'} 
         </button>
         <div className=' stopRegisterText'>
-        {is_waiting ? '대기 마감' : '이제 받을겨'} 
+        {is_waiting ? '대기 마감' : '대기 시작'} 
         </div>
       </div>
     </Container>
