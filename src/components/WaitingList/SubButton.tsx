e26779025733ec6903e06c91d7917e5fce82c5ca
“mineii” <kmaoo0406@naver.com>
2023-01-16 23:09:15 +0900

    feat: #12 대기 등록 마ㅁ useState 시도

--- src/components/WaitingList/SubButton.tsx
+++ src/components/WaitingList/SubButton.tsx
@@ -1,12 +1,22 @@
 import { Container } from "@material-ui/core";
-import React from "react";
+import { useState } from "react";
 import './WaitingList.scss';
 import {
   Link
 } from "react-router-dom"; 
+import axios from "axios";
 
 function SubButton() {
-  
+
+  const [store_id, setStore_id] = useState();
+  const [iswaiting, setIswaiting] = useState('');
+  const iswaitingFalse = () => {
+    axios.patch('http://localhost:8000//api/v1/stores/breaktimes/',{
+      store_id: store_id
+    })
+      .then( )
+  }
+
   return (
     <Container>
       <div id = "wrap">
