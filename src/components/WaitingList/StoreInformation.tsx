b40d398400eeed31f67dd4ac3936315972ae8f63
“mineii” <kmaoo0406@naver.com>
2023-01-17 18:19:00 +0900

    feat: #12 가게설명수정 시도

--- src/components/WaitingList/StoreInformation.tsx
+++ src/components/WaitingList/StoreInformation.tsx
@@ -1,8 +1,20 @@
-import * as React from 'react';
+import { useState } from 'react';
 import TextareaAutosize from '@mui/base/TextareaAutosize';
 import './StoreInformation.scss';
+import axios from 'axios';
 
-export default function StoreInformation() {
+function StoreInformation() {
+
+  const [inforamtion, setInformation] = useState(resInforamtion)  //resInforamtion : waitingList에서 넘겨준 데이터를 해당 컴포넌트에서 어떻게 부를건지, 받아온 데이터를 어떻게 부를건지
+  //const information = 
+  const storeinformationText = () => {
+    axios.patch('http://localhost:8000/api/v1/stores/details',{
+      store_id: localStorage.getItem("store_id"),
+      inforamtion: resInforamtion
+    }
+    )
+  }
+
   return (
     <div className='InformationOutline'>
       <TextareaAutosize
@@ -11,7 +23,14 @@ export default function StoreInformation() {
         aria-label="매장 소개 정보"
         placeholder="매장 소개글을 입력해주세요."
         className='InformationInput'
+        value =                  //value 기본값을 넣어주는 default데이터 ,
+                              //해당 페이지를 다시 열었을때 이전의 데이터가 뜨기 위해 axios로 받아온 데이터를 value로 넣어줘
+                              //Textarea에 있는 값은 이미 저장이 되어 있음, value값이 변함에 따라 return을 해주게 해야 함(밖으로 내뿜어주게)
+        onChange={e => setInformation(e.target.value)}  //value값이 바뀌는지 계속 추적해주는 함수, 값이 바뀌었을 때 실행, 이를 이벤트라고 함
+                              //event 발생하면 event 객체 내의 target.value에 바뀐 값이 있고 그 값이 setInformation으로, 
       />
     </div>
   );
 }
+
+export default StoreInformation;
--- src/pages/WaitingList/WaitingList.tsx
+++ src/pages/WaitingList/WaitingList.tsx
@@ -9,6 +9,10 @@ import TableTitle from "../../components/WaitingList/TableTitle";
 import Calender from "../../components/WaitingList/Calender";
 import StoreInformation from "../../components/WaitingList/StoreInformation";
 
+//대기자조회api 이 페이지에 연결되어야 함(useEffect) - store_id를 보내고 받아와야 함
+//store_id를 백엔드로 넘겨주면, 백엔드에서 is_waiting, waiting(리스트), information를 받아오기 때문에
+//이 information을 <StoreInformation/>에 넣어준 다음에 <TextareaAutosize를 호출할 때 value값으로 이 information을 넣어줘야 함
+
 
 function WaitingList() {
   document.body.style.backgroundColor = "#FFFBD9";
@@ -18,12 +22,13 @@ function WaitingList() {
       <div>
         <Logo/>
         <Calender/>
+        {/* axios 받아왔던 information정보들을 StoreInformation컴포넌트 호출할 때 넣어줘야해, 그래야 컴포넌트 값이 넘어가*/}
         <StoreInformation/>
       </div>
       <div className="VerticalLine"></div>
       <div>
         <TableTitle/>
-        <ListTable/>
+        <ListTable/> 
       </div>
       <SubButton/>
     </Container>
