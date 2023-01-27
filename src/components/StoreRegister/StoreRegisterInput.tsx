60dfe3763df2f71973a9f9fbda28fd0285ca452c
HAERYN <syt06119@naver.com>
2023-01-28 02:59:55 +0900

    feat: #16 주소 API 적용

--- src/components/StoreRegister/StoreRegisterInput.tsx
+++ src/components/StoreRegister/StoreRegisterInput.tsx
@@ -1,25 +1,44 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import './StoreRegisterInput.scss';
-import {
-  Link, useNavigate
-} from "react-router-dom"; 
+import {Link, useNavigate} from "react-router-dom"; 
+// import StoreLocation from "./StoreLocation";
 import axios from 'axios';
 
-//가게등록
+// declare global {
+//   interface Window {
+//     kakao: any;
+//   }
+// }
+
+// function A {
+//   latitude: 
+// }
 
 export default function RegisterInput() {
-
   const [name, setName] = useState('')
   const [phone_num, setPhone_num] = useState('')
-  const [posts, setPosts] = useState(0) //가게 주소
+  const [posts, setPosts] = useState('') //가게 주소
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
-
   const navigate = useNavigate();
+  const [location, setLocation] = useState(0);
+  const [latitude, setlatitude] = useState(0);
 
-  //로그인처럼!!
+  const storeSearch =()=>{
+    axios.get(`map-geocode/v2/geocode?query=장미로 101`,
+    {
+      headers: {
+        "X-NCP-APIGW-API-KEY-ID": "pg98qmcln0",
+        "X-NCP-APIGW-API-KEY": "ELFgrKAgPdLsxeQDxHBhqv1jQvrpZuVfDJ4Jcmrf"
+      }  
+    })
+      .then((response) => {
+        console.log(response.data)
+    })
+  }
+
 
   const storeRegister =()=>{
       //axios.post(url : post가 연결되어야 할 api주소, data : 백엔드에서 정의한 request body).then(앞 코드가 정상작동하면 실행되는 다음 행위)
@@ -34,9 +53,10 @@ export default function RegisterInput() {
           var str1 = 'Bearer '
           var token = str1.concat(response.data.access)
           localStorage.setItem('accessToken', token);
-          console.log('[가게등록access]' + token)
+          console.log(response.data.latitude)
+          console.log(response.data.longitude)
           navigate("/WaitingList")
-      }) 
+      })
   }
   //console.log(localStorage.getItem("id")) 로컬스토리지에서 저장되어있는 값을 꺼내와서 console.log에 출력해서 확인하는 코드(getItem)
 
@@ -52,12 +72,13 @@ export default function RegisterInput() {
         >
           <TextField onChange={(e)=>{setName(e.target.value)}} id="standard-basic" label="가게명 / 업소명" variant="standard" />
           <TextField  onChange={(e)=>{setPhone_num(e.target.value)}}id="standard-basic" label="가게 번호" variant="standard" />
-          <TextField onChange={(e)=>{setPosts(Number(e.target.value))}}id="standard-basic" label="가게 주소" variant="standard" />
+          <TextField onChange={(e)=>{setPosts((e.target.value))}}id="standard-basic" label="가게 주소" variant="standard" />
           <TextField onChange={(e)=>{setEmail(e.target.value)}}id="standard-basic" label="이메일 주소" variant="standard" />
           <TextField onChange={(e)=>{setPassword(e.target.value)}}id="standard-basic" label="비밀번호" variant="standard" />
         </Box>
+        
         <button onClick={storeRegister} className="ButtonStyle" >가게 등록</button>
+        <button onClick={storeSearch} className="ButtonStyle" >주소 테스트</button>
     </div>
-
   );
 }
